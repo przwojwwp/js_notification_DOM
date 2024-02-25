49854d42dcd6b37c5ca5dd6521a3f8753feabe2d
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-02-25 16:15:21 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+/* eslint-disable padding-line-between-statements */ "use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const titleElement = document.createElement("h2");
+    titleElement.classList.add("title");
+    titleElement.textContent = title;
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = description;
+    notification.appendChild(titleElement);
+    notification.appendChild(descriptionElement);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(165, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
